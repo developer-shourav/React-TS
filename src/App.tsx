a7ed92a5889ab98183351c1896c12bcfce7dc6fa
developer-shourav <mrinmoyraj823@gmail.com>
2024-06-17 15:38:53 +0600

    component object type added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Greet from './components/Greet'
+import Person from './components/Person'
 
 function App() {
+
+  const personName = {
+    firstName: "MD",
+    lastName:"Al Amin"
+  };
 
   return (
     <div className='App'>
       <Greet unreadMessageCount={5} name="Shourav" isLoggedIn={true} />
+      <Person name={personName} /> 
     </div>
   )
 }
