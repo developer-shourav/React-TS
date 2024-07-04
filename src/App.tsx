c27ec81dbc4026aca7b32afcc1deddf5e9e9b4a3
developer-shourav <mrinmoyraj823@gmail.com>
2024-07-05 01:23:24 +0600

    Generic type added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
-import { Private } from "./components/auth/Private";
-import { Profile } from "./components/auth/Profile";
+import { List } from "./components/generics/List";
+/* import { Private } from "./components/auth/Private";
+import { Profile } from "./components/auth/Profile"; */
 /* import { UserContextProvider } from "./components/context/UserContext";
 import User from "./components/context/User"; */
 /* import Box from "./components/context/Box";
@@ -54,49 +55,72 @@ function App() {
         <Heading>The Golden Golden Kela Awards Goes of Jayed Kan</Heading>
       </Oscar> */}
 
-
       {/* ----------------------Typing Event Props --------------- */}
       {/* <Button handleClick={(event, id) => {
         console.log("Button Clicked", event, id);
         
       }}></Button> */}
 
-     {/*  <Input value="" handleChange={(event) => console.log(event)
+      {/*  <Input value="" handleChange={(event) => console.log(event)
       } /> */}
-
 
       {/* ------------------Style Props------------------- */}
 
-     {/*  <Container cssStyle={{border:"2px dotted black", minWidth:"500px", minHeight:"300px", borderRadius:"20px", padding:"60px 0 0 0" }} /> */}
+      {/*  <Container cssStyle={{border:"2px dotted black", minWidth:"500px", minHeight:"300px", borderRadius:"20px", padding:"60px 0 0 0" }} /> */}
 
+      {/* -------------------Typing React Hooks (useState) ----------------------- */}
 
-     {/* -------------------Typing React Hooks (useState) ----------------------- */}
+      {/* <LoggedIn/> */}
 
-     {/* <LoggedIn/> */}
+      {/* -------------------Typing React Hook (useState) (Advanced) -------------- */}
 
-     {/* -------------------Typing React Hook (useState) (Advanced) -------------- */}
-      
-     {/*  <User/> */}
+      {/*  <User/> */}
 
-     {/* -------------------Typing React Hook (useReducer) (Advanced) -------------- */}
-     {/* <Counter/> */}
+      {/* -------------------Typing React Hook (useReducer) (Advanced) -------------- */}
+      {/* <Counter/> */}
 
-
-     {/* -------------------Typing React Hook (useContext) (Advanced) -------------- */}
-    {/*  <ThemeContextProvider>
+      {/* -------------------Typing React Hook (useContext) (Advanced) -------------- */}
+      {/*  <ThemeContextProvider>
        <Box/>
      </ThemeContextProvider> */}
 
-     {/* -------------------Typing React Hook (useContext) (Advanced) With Future Value-------------- */}
+      {/* -------------------Typing React Hook (useContext) (Advanced) With Future Value-------------- */}
 
-   {/*   <UserContextProvider>
+      {/*   <UserContextProvider>
       <User/>
      </UserContextProvider> */}
 
-
       {/* -------------------Typing Component Props (Advanced) -------------- */}
-      <Private isLoggedIn={true} component={Profile}></Private>
+      {/* <Private isLoggedIn={true} component={Profile}></Private> */}
 
+      {/* -------------------Typing Generic Props (Advanced) -------------- */}
+
+      {/* <List
+        items={["Batman", "Superman", "Wonder woman"]}
+        onClick={(item) => console.log(item)}
+      ></List>
+
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)}></List> */}
+
+
+      {/* -------Array List----- */}
+      <List
+        items={[
+          { id: 1,
+            first: "Shourav",
+            last: "Rajbongshi",
+          },
+          { id: 2,
+            first: "Ashim",
+            last: "Biswas",
+          },
+          { id: 3,
+            first: "Nirob",
+            last: "Paul",
+          },
+        ]}
+        onClick={(item) => console.log(item)}
+      ></List>
     </div>
   );
 }
